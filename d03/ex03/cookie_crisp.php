0c1983eb79c2687fde4304607258cde9e3bf5ffe
dgerard42 <daniella4422@gmail.com>
2018-02-23 22:43:17 -0800

    d03 done

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,4 +1,17 @@
 <?php
-	foreach ($_GET as $key => $i)
-		echo "$key: $i\n";
+	$action = $_GET["action"];
+	$name = $_GET["name"];
+
+	if ($action != null) {
+		if ("set" == $action) {
+			$value = $_GET["value"];
+			setcookie($name, $value, time() + (86400 * 30));
+		} else if ("get" == $action) {
+			echo $_COOKIE[$name] . "\n";
+		} else if ("del" == $action) {
+			setcookie($name, $value, time() - 42);
+		} else {
+			echo "error\n";
+		}
+	}
 ?>
